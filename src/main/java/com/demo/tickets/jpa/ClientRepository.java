5cea31a70a1817b9a964d3ae2eada99b804a64cb
Andrey Subbotin <a.subbotin@haulmont.com>
2024-05-22 13:12:34 +0400

    Initial version - Add Specification to client repository

--- src/main/java/com/demo/tickets/jpa/ClientRepository.java
+++ src/main/java/com/demo/tickets/jpa/ClientRepository.java
@@ -1,8 +1,9 @@
 package com.demo.tickets.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface ClientRepository extends JpaRepository<Client, UUID> {
+public interface ClientRepository extends JpaRepository<Client, UUID>, JpaSpecificationExecutor<Client> {
 }
